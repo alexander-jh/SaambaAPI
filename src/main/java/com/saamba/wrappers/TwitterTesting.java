cc316c9f035aa8dfa49cdf474fc3e544346fb61f
unknown <deuch.2@osu.edu>
2021-09-07 16:59:31 -0400

    made static method to get TweetList

--- src/main/java/com/saamba/wrappers/TwitterTesting.java
+++ src/main/java/com/saamba/wrappers/TwitterTesting.java
@@ -1,16 +1,13 @@
 package com.saamba.wrappers;
 
-import java.util.List;
-
 import io.github.redouane59.twitter.TwitterClient;
 import io.github.redouane59.twitter.dto.endpoints.AdditionalParameters;
 import io.github.redouane59.twitter.dto.tweet.TweetList;
-import io.github.redouane59.twitter.dto.tweet.TweetV2.TweetData;
 import io.github.redouane59.twitter.dto.user.User;
 import io.github.redouane59.twitter.signature.TwitterCredentials;
 
 public class TwitterTesting {
-    private static TwitterClient twitterClient;
+    private static final int RESULTS_TO_PULL = 20;
     //twitter credentials
     private static final String twitterAPIKey = "6kjRr6R2aUhIiLruVqJmkMqSm";
     private static final String twitterAPISecret = "jnDafGcWLCv5kPKDI251o2PcElCym2QaUn5MYcatHwRUGCDgan";
@@ -18,22 +15,37 @@ public class TwitterTesting {
     private static final String twitterAccessToken = "764298631931432960-CI6gcp7LljnvczIw39dmJPZKOilOsT8";
     private static final String twitterAccessTokenSecret = "scYrIE4CANo8kgKu8DhTU6N1mHeHmAQBXyUxL3n5EGrOg";
 
-    public static void main(String args[]) {
-        twitterClient = new TwitterClient(TwitterCredentials.builder()
-                .accessToken(twitterAccessToken)
-                .accessTokenSecret(twitterAccessTokenSecret)
-                .apiKey(twitterAPIKey).apiSecretKey(twitterAPISecret).build());
+    private static TwitterClient twitterClient = new TwitterClient(
+            TwitterCredentials.builder().accessToken(twitterAccessToken)
+                    .accessTokenSecret(twitterAccessTokenSecret)
+                    .apiKey(twitterAPIKey).apiSecretKey(twitterAPISecret)
+                    .build());
 
-        User ye = twitterClient.getUserFromUserName("kanyewest");
-        System.out.println(ye.getFollowersCount());
-        // System.out.println(ye.getPinnedTweet().getText());
+    public static TweetList getTweetInfo(String username) {
+        User u = twitterClient.getUserFromUserName(username);
         AdditionalParameters ap = AdditionalParameters.builder()
-                .recursiveCall(false).maxResults(20).build();
-        TweetList t = twitterClient.getUserTimeline(ye.getId(), ap);
-        List<TweetData> td = t.getData();
-        for (int i = 0; i < 15; i++) {
-            System.out.println(td.get(i).getText());
-        }
+                .recursiveCall(false).maxResults(RESULTS_TO_PULL).build();
+        TweetList t = twitterClient.getUserTimeline(u.getId(), ap);
+        return t;
 
     }
+
+    /*
+     * public static void main(String args[]) { twitterClient = new
+     * TwitterClient(TwitterCredentials.builder()
+     * .accessToken(twitterAccessToken)
+     * .accessTokenSecret(twitterAccessTokenSecret)
+     * .apiKey(twitterAPIKey).apiSecretKey(twitterAPISecret).build());
+     * 
+     * User ye = twitterClient.getUserFromUserName("kanyewest");
+     * System.out.println(ye.getFollowersCount()); //
+     * System.out.println(ye.getPinnedTweet().getText()); AdditionalParameters
+     * ap = AdditionalParameters.builder()
+     * .recursiveCall(false).maxResults(20).build(); TweetList t =
+     * twitterClient.getUserTimeline(ye.getId(), ap); List<TweetData> td =
+     * t.getData(); for (int i = 0; i < 15; i++) {
+     * System.out.println(td.get(i).getText()); }
+     * 
+     * }
+     */
 }
