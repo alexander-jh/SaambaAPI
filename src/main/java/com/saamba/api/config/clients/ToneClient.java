bc546e327f4565bf3f12bcf1b905a68252f5f98d
unknown <deuch.2@osu.edu>
2021-10-07 16:08:52 -0400

    added normalization to map

--- src/main/java/com/saamba/api/config/clients/ToneClient.java
+++ src/main/java/com/saamba/api/config/clients/ToneClient.java
@@ -89,7 +89,7 @@ public class ToneClient implements ClientConfig {
                 mp.replace(ts.get(j).getToneName(), mp.get(ts.get(j).getToneName()+ts.get(j).getScore()));
             }
         }
-
+        normMap(mp);
         return mp;
     }
 
