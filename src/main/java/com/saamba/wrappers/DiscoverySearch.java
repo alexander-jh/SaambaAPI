0ddc350b25349064f1dc015e7aa66a6dff5a14a3
Arnold <arnoldmakarov@yahoo.com>
2021-10-11 15:12:07 -0400

    Finished findSongs Method. Now goes into ArrayList

--- src/main/java/com/saamba/wrappers/DiscoverySearch.java
+++ src/main/java/com/saamba/wrappers/DiscoverySearch.java
@@ -9,22 +9,25 @@ import com.ibm.watson.discovery.v1.Discovery;
 import com.ibm.watson.discovery.v1.model.QueryOptions;
 import com.ibm.watson.discovery.v1.model.QueryResponse;
 import com.ibm.watson.discovery.v1.model.QueryOptions;
-import com.ibm.watson.discovery.v2.model.QueryResult;
+import com.ibm.watson.discovery.v1.model.QueryResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiscoverySearch {
-    private static String key = "BWC92BJnFoUgNaYZd1fBcAj1Zy1vaujX65d-RMbW6cpR";
+    private static String key = "W4yRKE--NoJJPHVMiOmu9LCWp5spX6Mqh5vbMi9mNlYT";
 //    private static String url = "https://api.kr-seo.discovery.watson.cloud.ibm.com/instances/f9f78f79-0354-4f71-95d1-cae2f2f2bbe7";
-    private static String url = "https://api.kr-seo.discovery.watson.cloud.ibm.com";
+    private static String url = "https://api.eu-gb.discovery.watson.cloud.ibm.com";
     //    private static ToneAnalyzer t = new ToneAnalyzer("2019-04-30",
 //            new IamAuthenticator.Builder().apikey(key).build());
 
-    public static String findSongs(String str) {
+    public static ArrayList<String[]> findSongs(String str) {
 
         Discovery discovery = new Discovery("2019-04-30", new IamAuthenticator.Builder().apikey(key).build());
-        discovery.setServiceUrl("https://api.kr-seo.discovery.watson.cloud.ibm.com");
+        discovery.setServiceUrl(url);
 
-        String environmentId = "53023a27-586f-403c-8443-df13cd7fdbcb";
-        String collectionId = "f491a294-9f6d-455c-aa2c-f3790ae0a37c";
+        String environmentId = "1545d966-b84a-4b43-aa4f-3ea72e60c85d";
+        String collectionId = "b56e44f2-f9e6-45cf-a756-d3ba136575de";
 
         // query document
         System.out.println("Querying the collection...");
@@ -33,11 +36,15 @@ public class DiscoverySearch {
         queryBuilder.naturalLanguageQuery("Happy");
         QueryResponse queryResponse = discovery.query(queryBuilder.build()).execute().getResult();
 
-        // print out the results
-        System.out.println("Query Results:");
-        System.out.println(queryResponse);
+        // Parses QueryResponse and puts Artist + Title in an ArrayList of Arrays [Artist, Title]
+        ArrayList<String[]> songs = new ArrayList<>();
+        List<QueryResult> results = queryResponse.getResults();
 
-        return "";
+        for (QueryResult result : results){
+            songs.add(new String[]{result.get("artist").toString(), result.get("title").toString()});
+        }
+
+        return songs;
     }
 
     public static void main(String args[]) {
@@ -45,7 +52,7 @@ public class DiscoverySearch {
 //
 //        System.out.println("Tone of the Twitter account: " + analysis);
 
-        System.out.println(findSongs("Happy"));
-
+        System.out.println(findSongs("Happy").get(1)[0]);
+//          findSongs("Happy");
     }
 }
